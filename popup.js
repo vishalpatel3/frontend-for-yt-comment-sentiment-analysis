6ebd423b667744f22dc22c691e96899b2519ffa3
unknown <ram3003200@gmail.com>
2025-04-04 12:49:28 +0530

    minor change

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const outputDiv = document.getElementById("output");
     const API_KEY = 'AIzaSyD_0qfXeFVqPqFx72gw_FIA_JsFGfdCwm4';  // Replace with your actual YouTube Data API key
-    const API_URL = 'http://my-elb-2062136355.us-east-1.elb.amazonaws.com:80';
+    const API_URL = 'http://192.168.1.123:5000';
   
     // Get the current tab's URL
     chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
